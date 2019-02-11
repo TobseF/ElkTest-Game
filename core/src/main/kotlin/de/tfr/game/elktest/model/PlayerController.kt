27fd68c1eaa03f524187a7db2a720f0e9c719751
Tobse <Tobse4Git@gmail.com>
2019-02-11 10:46:50 +0100

    🔧 Add player controller to test gamepad input

--- core/src/main/kotlin/de/tfr/game/elktest/model/PlayerController.kt
+++ core/src/main/kotlin/de/tfr/game/elktest/model/PlayerController.kt
@@ -1,14 +1,30 @@
 package de.tfr.game.elktest.model
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.controllers.Controller
 import com.badlogic.gdx.controllers.ControllerAdapter
+import com.badlogic.gdx.controllers.Controllers
 
 class PlayerController(val buttonIndex: Int, val action: () -> Unit) : ControllerAdapter() {
+
+    private companion object {
+        fun log(message: String) = Gdx.app.log("PlayerController", message)
+
+        init {
+            log("Found controllers:" + Controllers.getControllers())
+        }
+    }
+
+    init {
+        Controllers.addListener(this)
+    }
+
 
     override fun buttonDown(controller: Controller, buttonIndex: Int): Boolean {
         if (this.buttonIndex == buttonIndex) {
 
         }
+        log("Button $buttonIndex on Controller $controller")
         action.invoke()
         return false
     }
